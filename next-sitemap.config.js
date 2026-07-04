510834e882b03abe0fec372ccc143692f9ba42f6
Alexander Tartmin <alexander@tartmin.name>
2026-07-04 08:20:31 +0300

    feat(seo): prepare Bing and IndexNow discovery

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,7 +6,14 @@ module.exports = {
   // не індексуємо success (із query теж)
   exclude: ["/success", "/success*"],
   robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
+    policies: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "Bingbot", allow: "/" },
+      { userAgent: "DuckDuckBot", allow: "/" },
+      { userAgent: "OAI-SearchBot", allow: "/" },
+      { userAgent: "ChatGPT-User", allow: "/" },
+      { userAgent: "GPTBot", allow: "/" },
+    ],
     // sitemap автоматично додасться
   },
 };
